f3ed1c52036fc289ac9cf2cb8f717e82031e7b9a
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2023-04-24 16:51:35 +0000

    multi-page navbar created and is successful!

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div>
+            <h1>
+                Geeks for Geeks is a Computer Science portal for geeks.
+            </h1>
+        </div>
+    );
+};
+
+export default About;
